1c513e1e99d970d56fd6a425ab4560bffda746d5
Jason Arritt <jsn.arritt@gmail.com>
2022-02-24 14:38:09 -0500

    initial structure for collecting user inputs

--- script.js
+++ script.js
@@ -1,28 +1,83 @@
 // Assignment Code
+
+
+
+
+// function getLetters() {
+//   //Ask if they would like letters or not
+//   var letterConfirm = window.confirm("Would you like the password to contain letters?");
+//   //If they would like letters confirm uppercase/lowercase
+//   if (letterConfirm) {
+//     //Check about uppercase letters
+//     var uppercaseConfirm = window.confirm("Would you like to include uppercase letters in your password?");
+//     //Check about lowercase letters
+//     var lowercaseConfirm = window.confirm("Would you like to include lowercase letters in your password?");
+//   }
+// }
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-var generatePassword = function() {
-  //Collect input from user
-  //Collect desired number of characters
-   var numChars = window.prompt("How many characters would you like your password to be? (8-128)");
+//Collect input from user
+//Collect desired number of characters
+function getNumChars() {
+  numChars = window.prompt("How many characters would you like your password to be? (8-128)");
   if (numChars >= 8 && numChars <= 128) {
     console.log("That is a valid number of characters.");
   } else {
     window.alert("Please enter a value of no less than 8 and no more than 128 characters.");
-    generatePassword();
-  }
-  
-  //Ask if they would like letters or not
-  var letterConfirm = window.confirm("Would you like the password to contain letters?");
-  //If they would like letters confirm uppercase/lowercase
-  if (letterConfirm) {
-    //Check about uppercase letters
-    var uppercaseConfirm = window.confirm("Would you like to include uppercase letters in your password?");
-    //Check about lowercase letters
-    var lowercaseConfirm = window.confirm("Would you like to include lowercase letters in your password?");
-  }
+    getNumChars();
+  };
+  console.log("You have chosen " + numChars + " characters for your password.");
+  return numChars;
+};
 
+//Ask if uppercase letters are wanted
+function wantUppercase() {
+  uppercaseConfirm = window.confirm("Would you like to include uppercase letters in your password?");
+  console.log("You have set uppercase letters to " + uppercaseConfirm);
+  return uppercaseConfirm;
+}
+
+//Ask if lowercase letters are wanted
+function wantLowercase() {
+  lowercaseConfirm = window.confirm("Would you like to include lowercase letters in your password?");
+  console.log("You have set lowercase letters to " + lowercaseConfirm);
+  return lowercaseConfirm;
+}
+
+//Ask if number are wanted
+function wantNums() {
+  numConfirm = window.confirm("Would you like to include numbers in your password?");
+  console.log("You have set numbers included to " + numConfirm);
+  return numConfirm;
+}
+
+//Ask if symbols are wanted
+function wantSymbols() {
+  symConfirm = window.confirm("Would you like to include symbols in your password?");
+  console.log("You have set symbols included to " + symConfirm);
+  return symConfirm;
+}
+
+//Generate password
+function generatePassword() {
+  //Collect inputs
+  const numChars = getNumChars();
+  const uppercaseConfirm = wantUppercase();
+  const lowercaseConfirm = wantLowercase();
+  const numConfirm = wantNums();
+  const symConfirm = wantSymbols();
+
+
+  //Test return of inputs
+  console.log(numChars, uppercaseConfirm, lowercaseConfirm, numConfirm, symConfirm);
+
+
+  //Take inputs and generate password
 }
 
 generatePassword();
